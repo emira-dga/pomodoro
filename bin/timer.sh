c9a469b3a602796d2a82960e5d6f0ed103d98783
emira-dga <dga.emira12@gmail.com>
2025-05-12 16:19:33 +0200

    delete unsed echo messages and update config file

--- bin/timer.sh
+++ bin/timer.sh
@@ -10,19 +10,15 @@ total_cycle=$((hours_to_work * 2))
 cycle=1
 
 while [ $cycle -le $total_cycle ]; do
-  echo "pomo cycle $(($cycle)) started!!!"
   run_timer $work_duration 
   if (($cycle % 4 == 0)); then 
-#    echo " you can have a long break "
   notify-send "⏰ Pomodoro Done!" "Take a 10-minute break."
     run_timer $long_break
   else
     notify-send "⏰ Pomodoro Done!" "Take a 5-minute break."
- #   echo " sorry , its a short break "
     run_timer $short_break	
   fi
  ((cycle++))
 done
 
-  notify-send "⏰ Pomodoro Done!" "all your $hours_to_work hour(s) of Pomodoro complete!! "
-  #echo "All your $hours_to_work hour(s) of Pomodoro complete!"
+notify-send "⏰ Pomodoro Done!" "All your $hours_to_work hour(s) of Pomodoro complete!! "
--- lib/config.sh
+++ lib/config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 hours_to_work=${1}
-work_duration=1
-short_break=1
-long_break=1
+work_duration=25
+short_break=5
+long_break=15
